7307566d8ff417fab23d9ce072772847f7cc75a3
lonnstyle <lonnstyle@gmail.com>
2023-03-18 00:07:36 +0800

    fix: validation on booking

--- Service/Flask/src/Application/Api/latest/booking/schemas.py
+++ Service/Flask/src/Application/Api/latest/booking/schemas.py
@@ -10,10 +10,25 @@ class Booking(Schema):
 
     @validates('session')
     def validate_session(self, value):
-        if not flask_pymongo.db.sessions.find_one({'_id': ObjectId(value)}):
+        session = flask_pymongo.db.sessions.find_one({'_id': ObjectId(value)})
+        if session is None:
             raise ValidationError('Session does not exist', 'session')
+        bookings = flask_pymongo.db.booking.find({'session': ObjectId(value)}).count()
+        if bookings >= session['max_students']:
+            raise ValidationError('Session is full', 'session')
     
     @validates('user')
     def validate_user(self, value):
         if not flask_pymongo.db.users.find_one({'_id': ObjectId(value)}):
             raise ValidationError('User does not exist', 'user')
+        # check if the user has already booked this session
+        if flask_pymongo.db.booking.find_one({'user': ObjectId(value),'session': ObjectId(self.context['session'])}):
+            raise ValidationError('User has already booked this session', 'user')
+        
+    @post_load
+    def post_load(self, data):
+        # add the booking to the database
+        cnt = flask_pymongo.db.booking.insert_one(data).inserted_id
+        if cnt is None:
+            raise ValidationError('Booking not created', 'booking')
+        return {'payload': {'booking': {'_id': cnt}}}
